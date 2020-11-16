74e2598453b554be133fe5b002a86cc49a83b51a
Martin Thomson <mt@lowentropy.net>
2020-11-16 12:41:46 +1100

    Sort indexes

--- ietf109/index.md
+++ ietf109/index.md
@@ -1,3 +1,6 @@
 # ietf109
 
 * [agenda.md](./agenda.md)
+* [chairs.pdf](./chairs.pdf)
+* [conferencing.pdf](./conferencing.pdf)
+* [video.pdf](./video.pdf)
--- mkindex.sh
+++ mkindex.sh
@@ -7,13 +7,13 @@ exec 3>> README.md
 echo "# Meetings" 1>&3
 echo 1>&3
 
-for d in $(find . -maxdepth 1 -mindepth 1 -type d -print); do
+for d in $(find . -maxdepth 1 -mindepth 1 -type d -print | sort); do
     if [[ "${d#./}" != ".${d#./.}" ]]; then
         echo "* [${d##*/}]($d/)" 1>&3
 		exec 4> "$d"/index.md
 		echo "# ${d##*/}" 1>&4
 		echo 1>&4
-		for f in $(find "$d" -maxdepth 1 -mindepth 1 -type f \! -name index.md -print); do
+		for f in $(find "$d" -maxdepth 1 -mindepth 1 -type f \! -name index.md -print | sort); do
             echo "* [${f##*/}](./${f##*/})" 1>&4
 		done
 		exec 4>&-
